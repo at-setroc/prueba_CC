aadebc0917f555563b1675f19eed66d56825c0f2
Antonio Romero <at.romerocortes@gmail.com>
2022-10-03 02:33:00 +0200

    WIP

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,24 +3,47 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoryRepository;
+use App\Repository\FeatureRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends AbstractController
 {
     public function __construct(
-        private CategoryRepository $categoryRepository
+        private CategoryRepository $categoryRepository,
+        private FeatureRepository  $featureRepository
     ) {
     }
 
-    #[Route('/categories/{id}/features', name: 'app_form_features')]
-    public function findMainCategories(): Response
+    #[Route('/categories/{num}/features', name: 'app_form_features')]
+    public function formFeatures(Request $request): Response
     {
-        // TODO:......
+        $categoryId = $request->attributes->get("num");
         
-        // return new JsonResponse();
+        $category   = $this->categoryRepository->findOneby([
+            "id"        => $categoryId,
+            "isActive"  => true,
+            "hasForm"   => true
+        ]);
+
+        $features   = $this->featureRepository->findBy([
+            "category"  => $category,
+            "isActive"  => true
+        ],[
+            "formOrder" => "ASC"
+        ]);
+
+        // Si no encontramos la categoría o no hay características, redirigimos a la home
+        if (!$category || empty($features)) {
+            return $this->redirectToRoute("app_homepage");
+        }
+
+        return $this->render('category/form_features.html.twig', [
+            'category' => $category,
+            'features' => $features,
+        ]);
     }
 
 }
